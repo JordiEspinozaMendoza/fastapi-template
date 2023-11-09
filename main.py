a9194a313fb84128775aaf136328dbeb14b2b649
Jordi <jordi8101@gmail.com>
2023-11-09 00:37:09 -0800

    test

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File
 
 app = FastAPI()
 
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.post("/api/process-image/")
+def process_image(file: bytes = File(...)):
+    return {"file_size": len(file)}
